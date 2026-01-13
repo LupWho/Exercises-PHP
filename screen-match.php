7bf5d9d687d1c21e7e16beb25815ca9a3f9a723e
Westley Lopes <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>
2026-01-13 17:16:55 -0300

    screen match 0.1

--- screen-match.php
+++ screen-match.php
@@ -5,9 +5,17 @@ echo "Welcome to screen match!
 
 $nameMovie = "Top Gun - Maverick";
 $releaseMovie = 2022;
-$sumScore = 
-$scoreMovie = 9 + 6 + 7.5 + 5;
-$includedPackge = true;
 
-echo $nameMovie;
-echo $scoreMovie;
+$sumScore = 9;
+$sumScore += 6;
+$sumScore += 7.5;
+$sumScore += 9;
+$sumScore += 8;
+
+$scoreMovie = $sumScore / 5;
+$primePlan = true;
+
+$includedPackge = $primePlan || $releaseMovie <2020;
+
+echo "Name of the movie: " . $nameMovie . "\n";
+echo "Score movie: $scoreMovie\n";
