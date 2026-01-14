1a80bf30aac18522a37acf8190047ab70881c4f7
Wesley Lopes <wesleylopcrop@gmail.com>
2026-01-14 02:53:09 -0300

    Ex 05 - celsius conversor

---
+++ 01/14/celcius-conversor.php
@@ -0,0 +1,9 @@
+<?php
+## Exercise 05 - Write a PHP program that converts a temperature from Celsius to Fahrenheit.
+
+$celsius = 25; // Modifique esse valor para a temperatura que desejar
+$fahrenheit = $celsius * 1.8 + 32;
+
+$mensage = "the temperature $celsius Celsius is equivalent to $fahrenheit Fahrenheit";
+
+echo $mensage;
